46a9bcfb87e08e4bfc5fd759e07bd2bfd7b9915d
flitzcore <hemistergt@gmail.com>
2022-07-21 21:46:31 +0700

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:utime/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: WelcomePage());
   }
 }
---
+++ lib/welcome_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:utime/home_page.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Image.asset("images/desainwelcomepage.png"),
+                ),
+                const Text(
+                  "Are you having a tough day?\nIt's ok, take UTime",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w100),
+                ),
+                SizedBox(height: 20),
+                const Text("#YourMentalHealthMatters",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18, fontFamily: "Poppins")),
+              ],
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.black,
+              child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(_createRoute());
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const HomePage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(-1, 0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
